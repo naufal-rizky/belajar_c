9627b706d70b640ad0c2f70ac8917d66e6e57e4f
naufal-rizky <83692985+naufal-rizky@users.noreply.github.com>
2022-09-22 09:49:06 +0700

    Add files via upload

--- Jobsheet 2/soal_1.c
+++ Jobsheet 2/soal_1.c
@@ -1,99 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-    int menu, jumlah_barang;
-    float pembelian, diskon, harga;
+int main() {
+    double radius, keliling, luas;
+    double phi = 3.14;
 
-    printf("PILIHAN MENU\n");
-    printf("[1] BAKSO\n");
-    printf("[2] SOTO\n");
-    printf("[3] NASI GORENG\n");
-    printf("[4] MIE GORENG\n");
-    printf("PILIH MENU : ");
-    scanf("%d", &menu);
+    printf("Masukan jari-jari lingkaran: ");
+    scanf("%lf", &radius);
 
-    switch(menu)
-    {
-        case 1:
-            system("cls");
-            printf("Anda Memilih Bakso\n\n");
-            printf("Masukan Jumlah Yang Ingin Dibeli : ");
-            scanf("%d", &jumlah_barang);
+    keliling = 2 * phi * radius;
+    luas = 0.5 * phi * radius * radius;
 
-            harga       = 10000;
-            pembelian   = harga * jumlah_barang;
-
-            if(pembelian >= 100000)
-            {
-                diskon      = (pembelian * 5) / 100;
-                pembelian   = pembelian - diskon;
-            }
-
-            printf("TOTAL PEMBELIAN     : Rp%.f\n", pembelian);
-            printf("JUMLAH DISKON       : Rp%.f\n", diskon);
-            printf("TOTAL BAYAR         : Rp%.f\n", pembelian);
-            break;
-        case 2:
-            system("cls");
-            printf("Anda Memilih Soto\n\n");
-            printf("Masukan Jumlah Yang Ingin Dibeli : ");
-            scanf("%d", &jumlah_barang);
-
-            harga       = 15000;
-            pembelian   = harga * jumlah_barang;
-
-            if(pembelian >= 100000)
-            {
-                diskon      = (pembelian * 5) / 100;
-                pembelian   = pembelian - diskon;
-            }
-
-            printf("TOTAL PEMBELIAN     : Rp%.f\n", pembelian);
-            printf("JUMLAH DISKON       : Rp%.f\n", diskon);
-            printf("TOTAL BAYAR         : Rp%.f\n", pembelian);
-            break;
-        case 3:
-            system("cls");
-            printf("Anda Memilih Nasi Goreng\n\n");
-            printf("Masukan Jumlah Yang Ingin Dibeli : ");
-            scanf("%d", &jumlah_barang);
-
-            harga       = 20000;
-            pembelian   = harga * jumlah_barang;
-
-            if(pembelian >= 100000)
-            {
-                diskon      = (pembelian * 5) / 100;
-                pembelian   = pembelian - diskon;;
-            }
-
-            printf("TOTAL PEMBELIAN     : Rp%.f\n", pembelian);
-            printf("JUMLAH DISKON       : Rp%.f\n", diskon);
-            printf("TOTAL BAYAR         : Rp%.f\n", pembelian);
-            break;
-        case 4:
-            system("cls");
-            printf("Anda Memilih Mie Goreng\n\n");
-            printf("Masukan Jumlah Yang Ingin Dibeli : ");
-            scanf("%d", &jumlah_barang);
-
-            harga       = 10000;
-            pembelian   = harga * jumlah_barang;
-
-            if(pembelian >= 100000)
-            {
-                diskon      = (pembelian * 5) / 100;
-                pembelian   = pembelian - diskon;
-            }
-
-            printf("TOTAL PEMBELIAN     : Rp%.f\n", pembelian);
-            printf("JUMLAH DISKON       : Rp%.f\n", diskon);
-            printf("TOTAL BAYAR         : Rp%.f\n", pembelian);
-            break;
-        default:
-            printf("Pilihan Tidak Valid");
-            break;
-    }
-
+    printf("\nHasil lingkaran\n");
+    printf("Jari-jari   = %8.2lf\n", radius);
+    printf("Keliling    = %8.2lf\n", keliling);
+    printf("Luas        = %8.2lf\n", luas);
 }
