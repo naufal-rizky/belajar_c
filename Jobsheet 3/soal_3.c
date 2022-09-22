7a83a5e2786d4ed767ed279a72bc1cd9df28b63b
naufal-rizky <naufalrizky.nav@gmail.com>
2022-09-22 21:34:06 +0700

    third commit

--- Jobsheet 3/soal_3.c
+++ Jobsheet 3/soal_3.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main()
+{
+    int total;
+
+    for(int i = 10; i <= 100; i++){
+        total = total + i;
+        printf("%d+", i);
+    }
+    printf("\n\nTotal : %d ", total);
+}
