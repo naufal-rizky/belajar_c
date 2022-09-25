27435538bff09d180e6df8b5f22f3c281d288103
naufal-rizky <naufalrizky.nav@gmail.com>
2022-09-25 16:45:44 +0700

    add nomor 5

--- Jobsheet 3/soal_5.c
+++ Jobsheet 3/soal_5.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    system("cls");
+    int num, total = 0;
+
+    printf("Masukan Angka : ");
+    scanf("%d", &num);
+
+    for(int i = 1; i <= num; i++){
+        total = total + i;
+        printf("%d\n", i);
+    }
+    printf("Penjumlahan Bilangan Triangular : %d\n", total);
+}
